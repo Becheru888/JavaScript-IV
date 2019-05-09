df87fe336bf6f4c5f9b30de89ee848ca19697c79
Becheru888 <becheru93@gmail.com>
2019-05-09 15:01:04 +0100

    Lambda classes completed all the function called

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -40,7 +40,7 @@ class Student extends Person{
     }
 
     listSubjects(){
-        console.log(`${this.favSubject}` + `<br>`)
+        console.log(`${this.favSubject}`)
     }
 
     PRAssignment(subject){
@@ -51,14 +51,14 @@ class Student extends Person{
 
 
 class ProjectManager extends Instructor{
-    constructor(specialty, favLanguage, catchPhrase, gradClassName, favInstructor){
-        super(specialty, favLanguage, catchPhrase)
+    constructor(name, location, age, gender,specialty, favLanguage, catchPhrase, gradClassName, favInstructor){
+        super(name, location, age, gender,specialty, favLanguage, catchPhrase)
         this.gradClassName = gradClassName;
         this.favInstructor = favInstructor
     }
 
-    standUp(channel){
-        console.log(`${this.name} @channel standy time!`)
+    standUp(channel,name){
+        console.log(`${name} ${channel} @channel standy time!`)
     }
 
     debugCode(subject, student){
@@ -67,6 +67,36 @@ class ProjectManager extends Instructor{
 
 }
 
+
+const instructor1 = new Instructor('Gabe', 'UK', '31','Male','Js Dveloper','JavaScript','The force be with you.')
+const instructor2 = new Instructor('Adrian', 'Poland', '26','Male','Js Dveloper','Java','Learn Learn Learn')
+const student1 = new Student('Remi', 'London', '26', 'Male', 'Sales Consultant', 'FullStackWeb', 'JavaScript')
+const student2 = new Student('Victor', 'America', '21', 'Male', 'Student', 'FullStackWeb', 'Less, Css')
+const projectManager1 = new ProjectManager('Borja','Spain', '26' ,'Male','JavaDeveloper','Javascript','Just Relax','WEBEU2','Gabe')
+const projectManager2 = new ProjectManager('Sophie','UK', '29' ,'Female','C#Developer','C#','Make you homework','WEBEU2','Adrian')
+
+
+//Person Methodes called
+
+console.log(student1.speak())
+console.log(instructor1.speak())
+console.log(projectManager1.speak())
+
+//Intructor Methodes called
+
+instructor1.demo('JavaScript')
+instructor1.grade(student1,'Javascript')
+
+//Student Methodes called
+
+student1.listSubjects()
+student2.listSubjects()
+student1.PRAssignment('JavaScript')
+
+//ProjectManager Methodes called
+
+projectManager1.standUp('Remi','Help Channel')
+projectManager2.debugCode('JavaScript III',student1)
 
 
 
